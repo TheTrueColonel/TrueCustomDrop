618b47120509ade1d22e71f813cdf7da0b3a8ac4
Benjamin Sims <asschaps@outlook.com>
2017-07-05 12:21:16 -0500

    Fix Pom.xmls Start Sponge Development

--- TrueCustomDrops-Sponge/src/main/java/com/thetruecolonel/truecustomdrops/TrueCustomDropsSponge.java
+++ TrueCustomDrops-Sponge/src/main/java/com/thetruecolonel/truecustomdrops/TrueCustomDropsSponge.java
@@ -1,5 +1,8 @@
 package com.thetruecolonel.truecustomdrops;
 
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "truecustomdrops", name = "True Custom Drops", version = "1.0.0")
 public class TrueCustomDropsSponge {
 
 }
